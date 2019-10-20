afb48faff286c377effb218319093997e0256b06
Maksim_Smetanin <maks.s923@mail.ru>
2019-10-21 03:34:40 +0500

    complete router-link with application

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{open: isOpen}">
+    <router-link 
+    v-for="link of links"
+    :key="link.title"
+    tag="li"
+    :to="link.url"
+    active-class="active"
+    :exact="link.exact"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+
+  model: {
+    prop: 'isOpen'
+  },
+
+  props: {
+    isOpen: Boolean
+  },
+
+  data() {
+    return {
+      links: [
+        {title: 'Счет', url: '/', exact: true},
+        {title: 'История', url: '/history'},
+        {title: 'Планирование', url: '/planning'},
+        {title: 'Новая запись', url: '/record'},
+        {title: 'Категории', url: '/categories'},
+      ]
+    }
+  },
+}
+</script>

21b53665581beed14f22c8cfc67adcbe7fae6479
Maksim_Smetanin <maks.s923@mail.ru>
2019-10-21 04:27:19 +0500

    created validation inputs with components: login and register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,45 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="onSubmit">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
+
       <div class="input-field">
-        <input id="email" type="text" class="validate" />
+        <input
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{invalid: ($v.email.$dirty && !$v.email.required || $v.email.$dirty && !$v.email.email)}"
+        />
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.email.$dirty && !$v.email.required"
+        >Поле email не должно быть пустым</small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.email.$dirty && !$v.email.email"
+        >Введите корректный email</small>
       </div>
+
       <div class="input-field">
-        <input id="password" type="password" class="validate" />
+        <input
+          id="password"
+          type="password"
+          v-model="password"
+          :class="{invalid: ($v.password.$dirty && !$v.password.required || $v.password.$dirty && !$v.password.minLength)}"
+        />
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small
+          class="helper-text invalid"
+          v-if="$v.password.$dirty && !$v.password.required"
+        >Введите пароль</small>
+        <small
+          class="helper-text invalid"
+          v-else-if="$v.password.$dirty && !$v.password.minLength"
+        >Минимальная длина пароля {{$v.password.$params.minLength.min}} символов. Введено {{password.length}} символов</small>
       </div>
     </div>
+
     <div class="card-action">
       <div>
         <button class="btn waves-effect waves-light auth-submit" type="submit">
@@ -28,3 +55,46 @@
     </div>
   </form>
 </template>
+
+<script>
+import { email, required, minLength } from "vuelidate/lib/validators";
+import constants from "@/script/constants";
+
+export default {
+  name: "login",
+
+  data() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+
+  validations: {
+    email: {
+      required,
+      email
+    },
+    password: {
+      required,
+      minLength: minLength(constants.MINLENGTH)
+    }
+  },
+
+  methods: {
+    onSubmit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+
+      const formData = {
+        email : this.email,
+        password: this.password
+      }
+
+      this.$router.push("/");
+    }
+  }
+};
+</script>

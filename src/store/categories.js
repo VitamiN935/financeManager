6ea362fd067f7bd4094d94139ea162dd1c231e21
Maksim_Smetanin <maks.s923@mail.ru>
2019-10-21 23:54:52 +0500

    created logic category_create component and request to firebase

--- src/store/categories.js
+++ src/store/categories.js
@@ -7,7 +7,27 @@ export default {
         const uid = await dispatch('getUId');
         const category = await firebase.database().ref(`users/${uid}/categories`).push({ title, limit });
         return { title, limit, id: category.key }
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
 
+    async fetchCategories({ commit, dispatch }) {
+      try {
+        const uid = await dispatch('getUId');
+        const categories = (await firebase.database().ref(`users/${uid}/categories`).once('value')).val() || {};
+        return Object.keys(categories).map(key => ({...categories[key], id: key}));
+      } catch (e) {
+        commit('setError', e);
+        throw e;
+      }
+    },
+
+    async updateCategory({commit, dispatch}, {id, title, limit}) {
+      try {
+        const uid = await dispatch('getUId');
+        await firebase.database().ref(`/users/${uid}/categories`).child(id).update({title, limit});
       } catch (e) {
         commit('setError', e);
         throw e;
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,35 +4,52 @@
       <h3>Категории</h3>
     </div>
     <section>
-      <div class="row">
-        <CategoriesCreate @created='addNewCategory'/>
-        <CategoriesEdit />
+      <Loader v-if="loading"/>
+      <div class="row" v-else>
+        <CategoryCreate @created='addNewCategory'/>
+        <CategoryEdit :categories='categories' @update='updateCategories' :key="categories.length + updateCount"/>
       </div>
     </section>
   </div>
 </template>
 
 <script>
-import CategoriesCreate from '@/components/CategoriesCreate'
-import CategoriesEdit from '@/components/CategoriesEdit'
+import CategoryCreate from '@/components/CategoryCreate'
+import CategoryEdit from '@/components/CategoryEdit'
+import Loader from '@/components/app/Loader'
 
 export default {
   name: 'categories',
 
   components: {
-    CategoriesCreate,
-    CategoriesEdit,
+    CategoryCreate,
+    CategoryEdit,
+    Loader
   },
 
   data() {
     return {
-      categories: []
+      categories: [],
+      loading: true,
+      updateCount: 0,
     }
+  },
+
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories');
+    this.loading = false;
   },
 
   methods: {
     addNewCategory(category) {
       this.categories.push(category);
+    },
+
+    updateCategories(category) {
+      const idx = this.categories.findIndex(c => c.id === category.id);
+      this.categories[idx].title = category.title;
+      this.categories[idx].limit = category.limit;
+      this.updateCount++
     }
   },
 }
